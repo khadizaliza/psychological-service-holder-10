d9017680ec875b37943e3278af3c687364205507
khadizaliza <khadizaakter19836@gmail.com>
2022-04-18 15:47:31 +0600

    navbar-create

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,26 @@
 
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import doctor5 from '../../../image/doctor5.jpg'
 
 const Header = () => {
     return (
         <header>
-          <h2>this is header</h2>  
+          <>
+  
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home"><img src={doctor5} height="50px"  alt="" /></Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+ 
+</>
         </header>
     );
 };
